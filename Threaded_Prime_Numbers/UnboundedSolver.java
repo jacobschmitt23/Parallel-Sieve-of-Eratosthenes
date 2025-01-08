3a8c9c1b274f4233fd7bf55c55583f9f4d1722e6
jacobschmitt23 <159497424+jacobschmitt23@users.noreply.github.com>
2025-01-08 18:12:34 -0500

    Create UnboundedSolver.java

---
+++ Threaded_Prime_Numbers/UnboundedSolver.java
@@ -0,0 +1,153 @@
+package Threaded_Prime_Numbers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Unbounded Solver Sieve of Eratosthenes Solver
+ * 
+ * @author Jacob Schmitt
+ */
+public class UnboundedSolver {
+
+    // @GuardedBy("this")
+    private boolean[] primes;
+    private int numPrimes;
+    private int MAX_SQRT = (int)Math.sqrt(Integer.MAX_VALUE);
+
+    /**
+     * Menu display for the unbounded solver Sieve of Eratosthenes that allows input of the upper bound for the prime number calculations
+     * 
+     * @param scanner - allows for reading from the command prompt to take in the upper bound of the range from the user
+     * @throws InterruptedException 
+     */
+    public void menu(Scanner scanner) {
+        
+        // Initial menu display
+        System.out.println("\nWelcome to the Unbounded Solver Sieve of Eratosthenes Solver");
+        System.out.println("Please enter a number and we will return the amount of prime numbers between 1 and that number (inclusive). Enter 0 to return to the main menu.");
+
+        while (true) {
+            int num;
+
+            // Ensure proper input and if not change the number for error messages
+            try {
+                num = Integer.parseInt(scanner.nextLine());
+                if (num < 0) {
+                    num = -1;
+                }
+            } catch (NumberFormatException e) {
+                num = -1;
+            }
+
+            // Based off input return to first main menu, print an error, or perform calculation
+            if (num == 0) {
+                return;
+            } else if (num == -1) {
+                System.out.println("Invalid input, try again");
+            } else {
+                long startTime = System.nanoTime();
+                int numPrimes = solve(num);
+                long endTime = System.nanoTime();
+                System.out.println("There are " + numPrimes + " prime numbers between 1 and " + num);
+                System.out.println("It took " + ((endTime - startTime) / 100000.0) + " milliseconds to calculate");
+            }
+        }
+
+    }
+
+    /**
+     * Solves the Sieve of Eratosthenes in an unbounded solver manner
+     * 
+     * @param num - the upper bound of the range of primes you are trying to find. This number is inclusive in the number of primes calculated
+     * @return - the number of primes in the inclusive range from 1 to num
+     */
+    private int solve(int num) {
+        numPrimes = 0;
+        // Initialize the prime array
+        primes = new boolean[num-1];
+        for (int i = 0; i < num-1; i++) {
+            primes[i] = true;
+        }
+        List<Thread> threads = new ArrayList<>();
+        int numThreads = Runtime.getRuntime().availableProcessors()+1;
+        int rangeOfThreads = (num-1) / numThreads;
+        int rangeStart = 0;
+        int rangeEnd = rangeOfThreads;
+        // Create threads and execute their tasks
+        for (int i = 0; i < numThreads; i++) {
+            Runnable task = new Task(rangeStart,rangeEnd);
+            Thread thread = new Thread(task);
+            threads.add(thread);
+            thread.start();
+            rangeStart += rangeOfThreads;
+            if (i == numThreads-2) {
+                rangeEnd = num-1;
+            } else {
+                rangeEnd += rangeOfThreads;
+            }
+        }
+        // Ensures all tasks are completed
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // Count the number of primes
+        for (boolean isPrime : primes) {
+            if (isPrime) {
+                numPrimes++;
+            }
+        }
+        return numPrimes;
+    }
+
+    /**
+    * Runnable task that will be completed by the threads
+    * 
+    * @author Jacob Schmitt
+    */
+    private class Task implements Runnable {
+
+        private int startRange;
+        private int endRange;
+
+        /**
+         * Constructor for Task object 
+         * 
+         * @param startRange - starting range of the prime array for this thread to work on
+         * @param endRange - ending range of the prime array for this thread to work on
+         */
+        public Task(int startRange, int endRange) {
+            this.startRange = startRange;
+            this.endRange = endRange;
+        }
+
+        /**
+         * The specific task that each thread will be completing. This is the manipulation of the prime array to mark multiples of prime numbers.
+         */
+        @Override
+        public void run() {
+            for (int i = startRange; i < endRange; i++) {
+                int multiple = i+2;
+                if (primes[i]) {
+                    // Ensure no integer overflow
+                    int startingMultiple = multiple+multiple;
+                    if (multiple <= MAX_SQRT) {
+                        startingMultiple = multiple*multiple;
+                    }
+                    // Critical region where shared data is manipulated
+                    synchronized (UnboundedSolver.this) {
+                        for (int j = startingMultiple; j-2 < primes.length; j += multiple) {
+                            primes[j-2] = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
